88e11b54299cf40f4b99864d335ed4baa01e4963
Hernan David <hernan.plazabs@gmail.com>
2022-05-18 10:54:54 -0500

    [UPDATE] images routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "../src/sass/app.scss";
-import logo from "./assets/images/photosnap.svg";
+
 import Jobs from "./components/jobs/Index";
 
 function App() {
   return (
     <div className="appContainer">
-      <img src={logo} alt="" />
       <header className="bg-header"></header>
       <div className="bg-subheader"></div>
       <Jobs />
--- src/context/jobs/JobsContext.tsx
+++ src/context/jobs/JobsContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 import { IJob, IJobcontext } from "./interfaces";
-import dataJobs from "../../data.json";
+import dataJobs from "../../data";
 import useGetJobs from "../../hooks/useGetJobs";
 
 export const JobsState = createContext({} as IJobcontext);
@@ -9,7 +9,7 @@ const JobsContext = ({ children }: { children: JSX.Element }) => {
   // const [jobs, setJobs] = useState<IJob[]>(dataJobs);
   const [filterValues, setFilterValues] = useState<string[]>([]);
   const { jobsRes } = useGetJobs({ jobs: dataJobs, filterValues });
-
+  console.log(dataJobs);
   const handleSetFilterValues = (language: string) => {
     setFilterValues((prev) => {
       let newFilterValues: string[] = [];
